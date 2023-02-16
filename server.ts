c79873a3fcec877348f030438daf310a8aabdde0
Rohit Sanwariya <rohitsanwariya1995@gmail.com>
2023-02-16 22:07:32 +0530

    mongoose with dotenv connected

--- server.ts
+++ server.ts
@@ -1,14 +1,24 @@
 import express, { Application } from 'express';
 import AuthRouter from './Routes/Auth'
+import { Mongoose } from 'mongoose'
+import "dotenv/config";
 
-const app:Application = express();
-const PORT:number = 3000;
+const app: Application = express();
+const mongoose = new Mongoose()
+
+const PORT: string | undefined = process.env.PORT;
+const MONGO_URI: string | undefined = process.env.MONGO_URI || "";
 
 app.use(express.json());
 
-app.use('/auth',AuthRouter)
+app.use('/auth', AuthRouter)
 
 
-app.listen(PORT,()=>{
-    console.log(`port listening on ${PORT}`)
+app.listen(PORT, async () => {
+    console.log(`port listening on ${PORT}`);
+    try {
+        await mongoose.connect(MONGO_URI);
+    } catch (error) {
+        console.log(error)
+    }
 })
